d8edad1485b8e374e52838149366c06b8738ea1b
jycouet <jycouet@gmail.com>
2024-10-28 13:35:37 +0100

    fixing ssr & fetch

--- src/routes/todos/+page.ts
+++ src/routes/todos/+page.ts
@@ -3,12 +3,9 @@ import type { PageLoad } from "./$types";
 import { Task } from "../../demo/todo/Task";
 
 export const load = (async (event) => {
-  // remult.apiClient.httpClient = event.fetch;
-
-  // remult.dataProvider = new RestDataProvider(() => ({
-  //   httpClient: event.fetch,
-  // }));
-
-  const todos = await remult.repo(Task).find();
+  const dp = new RestDataProvider(() => ({
+    httpClient: event.fetch,
+  }));
+  const todos = await remult.repo(Task, dp).find();
   return { todos };
 }) satisfies PageLoad;
